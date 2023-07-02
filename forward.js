f1548f467f3b063f68a07f8c437650b9e6329046
texiwustion <w6666s@outlook.com>
2023-07-02 13:46:28 +0800

    ✨ feat(forward): 创建转发器

---
+++ forward.js
@@ -0,0 +1,22 @@
+const forward = {
+    add: function (key) { // 转发即将被 unmap 的键
+        return api.map(`for${key}`, key)
+    },
+    cancel: function (key) { // 删除转发生成的键
+        api.unmap(`for${key}`)
+        api.unmap(key)
+    },
+    use: function (key) {
+        return `for${key}`
+    }
+}
+
+const colemak = {
+    forward: function (key) { // 转发即将被 unmap 的键
+        api.map(key, `col${key}`)
+        api.unmap(`col${key}`)
+    },
+    use: function (key) {
+        return `col${key}`
+    }
+}
