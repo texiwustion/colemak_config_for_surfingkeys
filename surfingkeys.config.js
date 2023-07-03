f400e1200a0e033a5c5d5b9aab6e72310ee60bf7
texiwustion <w6666s@outlook.com>
2023-07-03 08:14:38 +0800

    ✨ feat(page_browse): 完成功能开发

--- surfingkeys.config.js
+++ surfingkeys.config.js
@@ -63,7 +63,7 @@ const mapLists = {
     'ge': 'G',
     'N': '0',
     'I': '$',
-    /// tabs
+    /// tabs              t
     // 垂直标签习惯
     'ty': 'yt',
     'tY': 'yT',
@@ -84,13 +84,25 @@ const mapLists = {
     'zu': 'zi',
     'zo': 'ze',
     'zz': 'zr',
-    // mouse_click 鼠标点击     
+    // mouse_click 鼠标点击    f 
     'fn': 'f',
     'fi': 'cf',
     'fe': 'af',
     'fu': 'gf',
     'ff': 'q',
-    'F': ';di'
+    'F': ';di',
+    // page_browse 网页浏览   p
+    'pU': 'gT',  //最早的标签
+    'pE': 'gt',
+    'pp': 'gp', //play tab
+    'p?': 'g?', //移除网址的 ? 查询参数
+    'p#': 'g#', //移除 # 后
+    'pr': 'gU', //跳到网址的根路径
+    'pk': ';U', //vim 编辑网址 并刷新
+    'pK': ';u', //vim 编辑网址 并在新标签打开
+    'pu': 'B', //回到前一个标签
+    'pn': 'S', //页面后退
+    'pi': 'D', //页面前进
 }
 
 forwardFactory.push(mapLists)
